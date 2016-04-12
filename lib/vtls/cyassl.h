0ab3c5ee7ba149e4fbd6ca3ec8f6e4d1432817f9
Henri <henri@granitetower.net>
2016-04-12 10:33:54 -0700

    Fixed typo in comment.

--- lib/vtls/cyassl.h
+++ lib/vtls/cyassl.h
@@ -47,7 +47,7 @@ void Curl_cyassl_sha256sum(const unsigned char *tmp, /* input */
                      unsigned char *sha256sum, /* output */
                      size_t unused);
 
-/* Set the API backend definition to Schannel */
+/* Set the API backend definition to CYA */
 #define CURL_SSL_BACKEND CURLSSLBACKEND_CYASSL
 
 /* this backend supports CURLOPT_SSL_CTX_* */
